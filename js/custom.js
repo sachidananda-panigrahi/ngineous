b896c82ee5927e98948b6c5ff31cf2a4c73e9f77
sachidananda panigrahi <sachidananda.panigrahi@weboniselab.com>
2014-10-17 22:19:34 +0530

    responsive menu

--- js/custom.js
+++ js/custom.js
@@ -11,6 +11,18 @@ function headerFix(){
 
     });
 }
+// Show Collapse Nav Click function
+function navClick(){
+    $(".responsiveMenu").on('click',function(event){
+        event.preventDefault();
+        if($(".menu").css('display') == 'none'){
+            $(".menu").slideDown();
+        }
+        else{
+            $(".menu").slideUp();
+        }
+    });
+}
 //Click Scroll
 function sectionScroll(){
     $( '.mainNav > li > a, .roundedMenu li a' ).on('click', function(event) {
@@ -25,9 +37,12 @@ function sectionScroll(){
 function getActiveSection(){
     var navBar = $('.staticNav');
     var mainNav = $('.mainNav');
+    var responsiveMenu = $('.responsiveMenu');
     navBar.removeClass("fixedNav");
     $('.mainNav li').removeClass('active');
     mainNav.hide();
+    responsiveMenu.hide();
+    var winWidth = $(window).width();
     var winScrollPos = $(window).scrollTop();
 
     if($('#howItWork').offset().top <= winScrollPos && winScrollPos < $('#aboutSec').offset().top)
@@ -35,35 +50,49 @@ function getActiveSection(){
         $('.mainNav li:nth-child(1)').addClass('active');
         navBar.addClass("fixedNav");
         mainNav.show();
-
+        if(winWidth < 768){
+            responsiveMenu.show();
+        }
     }
     else if($('#aboutSec').offset().top <= winScrollPos && winScrollPos < $('#subscription').offset().top)
     {
         $('.mainNav li:nth-child(2)').addClass('active');
         navBar.addClass("fixedNav");
         mainNav.show();
+        if(winWidth < 768){
+            responsiveMenu.show();
+        }
     }
     else if($('#subscription').offset().top <= winScrollPos && winScrollPos < $('#contactSec').offset().top)
     {
         $('.mainNav li:nth-child(3)').addClass('active');
         navBar.addClass("fixedNav");
         mainNav.show();
+        if(winWidth < 768){
+            responsiveMenu.show();
+        }
     }
     else if($('#contactSec').offset().top <= winScrollPos )
     {
         $('.mainNav li:nth-child(4)').addClass('active');
         navBar.addClass("fixedNav");
         mainNav.show();
+        if(winWidth < 768){
+            responsiveMenu.show();
+        }
     }
 
 
 }
 $(document).ready(function(){
-
+    $('.responsiveMenu').hide();
+    navClick();
     sectionScroll();
     sectionHeight();
     headerFix();
     $(window).resize(function(){
         sectionHeight();
+        sectionScroll();
+        headerFix();
     })
 })
