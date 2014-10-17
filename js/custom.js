bf6acdce28aab2760cee7fe87377c862f97f4789
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2014-10-18 01:39:19 +0530

    working on responsive menu

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 function sectionHeight(){
     var height = $(window).height();
-    $('.mainWrap section').css({'min-height': height-49});
+    $('.mainWrap section').css({'min-height': height});
     $('.mainWrap .landing').css({'min-height': height});
-    $('.howItWorksDetSec, .innerCnt').css({'height': height-49});
+    $('.howItWorksDetSec, .innerCnt').css({'height': height});
 
 }
 function headerFix(){
-//    var height = $(window).height();
+
     $(document).on("scroll", function () {
         getActiveSection();
 
@@ -14,8 +14,9 @@ function headerFix(){
 }
 // Show Collapse Nav Click function
 function navClick(){
-    $(".responsiveMenu").on('click',function(event){
-        event.preventDefault();
+    $(".responsiveMenu, .hideOnclick a").on('click',function(event){
+        event.stopImmediatePropagation();
+//        alert('hi')
         if($(".menu").css('display') == 'none'){
             $(".menu").slideDown();
         }
@@ -30,53 +31,80 @@ function sectionScroll(){
         event.preventDefault();
         var target = "#" + $(this).data('href');
         $('html, body').animate({
-            scrollTop: $(target).offset().top - 49
+            scrollTop: $(target).offset().top
         }, 1000);
     });
 }
 //Active Nav
 function getActiveSection(){
+    var width = $(window).width();
     var navBar = $('.staticNav');
     var mainNav = $('.mainNav');
     navBar.removeClass("fixedNav");
     $('.mainNav li').removeClass('active');
-    mainNav.hide();
+    if(width > 768){
+        mainNav.hide();
+    }
     var winScrollPos = $(window).scrollTop();
+    winScrollPos = winScrollPos + 49;
+    console.log('scroll pos'+ winScrollPos);
+    console.log('contactSec offset'+ $('#contactSec').offset().top);
 
     if($('#howItWork').offset().top <= winScrollPos && winScrollPos < $('#aboutSec').offset().top)
     {
         $('.mainNav li:nth-child(1)').addClass('active');
         navBar.addClass("fixedNav");
-        mainNav.show();
+        if(width > 768){
+            mainNav.show();
+        }
 
     }
     else if($('#aboutSec').offset().top <= winScrollPos && winScrollPos < $('#subscription').offset().top)
     {
         $('.mainNav li:nth-child(2)').addClass('active');
         navBar.addClass("fixedNav");
-        mainNav.show();
+        if(width > 768){
+            mainNav.show();
+        }
     }
     else if($('#subscription').offset().top <= winScrollPos && winScrollPos < $('#contactSec').offset().top)
     {
         $('.mainNav li:nth-child(3)').addClass('active');
         navBar.addClass("fixedNav");
-        mainNav.show();
+        if(width > 768){
+            mainNav.show();
+        }
     }
     else if($('#contactSec').offset().top <= winScrollPos )
     {
         $('.mainNav li:nth-child(4)').addClass('active');
         navBar.addClass("fixedNav");
-        mainNav.show();
+        if(width > 768){
+            mainNav.show();
+        }
     }
 
 
 }
+
+function belowIpadSection(){
+    var width = $(window).width();
+    if(width < 768)
+    {
+        $('.mainNav').addClass('hideOnclick');
+        navClick();
+    }
+}
+
 $(document).ready(function(){
     navClick();
     sectionScroll();
     sectionHeight();
     headerFix();
+    belowIpadSection();
     $(window).resize(function(){
         sectionHeight();
+        headerFix();
+        belowIpadSection();
     })
 })
